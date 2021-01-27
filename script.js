36c4520192a7c77f16348b5bd65813530fa867b9
devsfe <fernando.chaves16@hotmail.com>
2021-01-27 07:13:11 -0300

    Projeto finalizado ?

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-btnGenerate = document.getElementById('generate-btn');
+const btnGenerate = document.getElementById('generate-btn');
+const container = document.getElementById('container');
+const personName = document.getElementById('name');
+const personImage = document.getElementById('personImage');
+const personAge = document.getElementById('age');
+const personEmail = document.getElementById('email');
+const personCountry = document.getElementById('country');
 
-getRandomPerson();
-getRandomPerson();
+let data = new Array;
 
-//Fetch random person
+//Fetch and get random person
 async function getRandomPerson() {
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
@@ -12,8 +17,33 @@ async function getRandomPerson() {
 
     const newUser = {
         picture: `${user.picture.large}`,
-        name: `${user.name.first} ${user.name.last}`
+        name: `${user.name.first} ${user.name.last}`,
+        age: `${user.dob.age}`,
+        email: `${user.email}`,
+        country: `${user.location.country}`
     } 
 
-    console.log(data);
+    addData(newUser);
 } 
+
+// Add new obj to data arr
+function addData(obj) {
+    data.push(obj);
+
+    updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+
+    data.forEach(function(element) {
+       personName.innerText = 'Name: ' + element.name; 
+       personImage.src = element.picture;
+       personAge.innerText = 'Age: ' + element.age;
+       personEmail.innerText = 'Email: ' + element.email;
+       personCountry.innerText = 'Country: ' + element.country;
+    });
+}
+
+
+btnGenerate.addEventListener('click', getRandomPerson);
